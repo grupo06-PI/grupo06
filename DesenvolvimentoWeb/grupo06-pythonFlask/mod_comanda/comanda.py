479004afd497788c4c935f5294c92780129ce4da
nicoooadams <contato.nicoadams1@hotmail.com>
2020-10-18 13:53:20 -0300

    Criação de mais algumas telas/campos

--- DesenvolvimentoWeb/grupo06-pythonFlask/mod_comanda/comanda.py
+++ DesenvolvimentoWeb/grupo06-pythonFlask/mod_comanda/comanda.py
@@ -2,6 +2,6 @@ from flask import Blueprint, render_template
 
 bp_comanda = Blueprint('comanda', __name__, template_folder='templates', url_prefix='/comandas')
 
-@bp_comanda.route("/cadComanda", methods=['GET'])
-def formComanda():
+@bp_comanda.route("/AbrirComanda", methods=['GET'])
+def AbrirComanda():
     return render_template("formComanda.html")
