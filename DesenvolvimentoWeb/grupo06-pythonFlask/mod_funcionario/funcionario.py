4a68be2954da792cc9a8f71b99bed254682186da
nicoooadams <contato.nicoadams1@hotmail.com>
2020-11-02 15:40:06 -0300

    crud funcionarios com ajax fix #15

--- DesenvolvimentoWeb/grupo06-pythonFlask/mod_funcionario/funcionario.py
+++ DesenvolvimentoWeb/grupo06-pythonFlask/mod_funcionario/funcionario.py
@@ -1,12 +1,91 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, redirect, url_for, flash, jsonify
+
+from mod_funcionario.funcionarioBD import Funcionarios
 
 bp_funcionario = Blueprint('funcionario', __name__, template_folder='templates', url_prefix="/funcionarios")
 
-@bp_funcionario.route("/cadFuncionario", methods=['GET'])
+
+@bp_funcionario.route("/cadFuncionario", methods=['GET', 'POST'])
 def CadFuncionario():
-    return render_template("formFuncionario.html")
+    funcionario = Funcionarios()
+    return render_template("formFuncionario.html", funcionario=funcionario, content_type='application/json')
 
 
-@bp_funcionario.route("/listaFuncionarios", methods=['GET'])
+@bp_funcionario.route("/listaFuncionarios", methods=['GET', 'POST'])
 def ListaFuncionarios():
-    return render_template("formListaFuncionarios.html")
+    funcionario = Funcionarios()
+    res = funcionario.selectALL()
+    return render_template("formListaFuncionarios.html", funcionario=funcionario, result=res, content_type='application/json')
+
+
+@bp_funcionario.route("/formEditFuncionario", methods=['POST'])
+def formEditFuncionario():
+
+    funcionario = Funcionarios()
+    funcionario.id_funcionario = request.form['id_funcionario']
+    funcionario.selectONE()
+    return render_template('formFuncionario.html', funcionario=funcionario, content_type='application/json')
+
+
+
+
+@bp_funcionario.route("/addFuncionario", methods=['GET','POST'])
+def addFuncionario():
+    _msg = ""
+
+    try:
+        funcionario=Funcionarios()
+
+        funcionario.id_funcionario = request.form['id_funcionario']
+        funcionario.nome = request.form['nome']
+        funcionario.matricula = request.form['matricula']
+        funcionario.cpf = request.form['cpf']
+        funcionario.telefone = request.form['telefone']
+        funcionario.grupo = request.form['grupo']
+        funcionario.senha = request.form['senha']
+
+        _msg = funcionario.insert()
+        return jsonify(erro=False, mensagem=_msg)
+
+    except Exception as e:
+        _msg, _msg_excpetion = e.args
+        return jsonify(erro=True, mensagem=_msg, mensagem_exception=_msg_excpetion)
+
+@bp_funcionario.route("/editFuncionario", methods=['POST'])
+def editFuncionario():
+        _msg = ""
+
+        try:
+            funcionario=Funcionarios()
+
+            funcionario.id_funcionario = request.form['id_funcionario']
+            funcionario.nome = request.form['nome']
+            funcionario.matricula = request.form['matricula']
+            funcionario.cpf = request.form['cpf']
+            funcionario.telefone = request.form['telefone']
+            funcionario.grupo = request.form['grupo']
+            funcionario.senha = request.form['senha']
+
+            _msg = funcionario.update()
+            return jsonify(erro=False, mensagem=_msg)
+
+        except Exception as e:
+            _msg, _msg_excpetion = e.args
+            return jsonify(erro=True, mensagem=_msg, mensagem_exception=_msg_excpetion)
+
+
+@bp_funcionario.route("/deleteFuncionario", methods=['POST'])
+def deleteFuncionario():
+    _msg = ""
+    try:
+
+        funcionario = Funcionarios()
+        funcionario.id_funcionario = request.form['id_funcionario']
+
+        _msg = funcionario.delete()
+
+        return jsonify(erro=False, mensagem=_msg)
+
+    except Exception as e:
+        _msg, _msg_exception = e.args
+        return jsonify(erro=True, mensagem=_msg, mensagem_exception=_msg_exception)
---
+++ DesenvolvimentoWeb/grupo06-pythonFlask/mod_funcionario/funcionarioBD.py
@@ -0,0 +1,162 @@
+from BancoBD import Banco
+
+
+class Funcionarios(object):
+    def __init__(self, id_funcionario=0, nome="", matricula="", cpf="", telefone="", grupo="", senha=""):
+
+        self.id_funcionario = id_funcionario
+        self.nome = nome
+        self.matricula = matricula
+        self.cpf = cpf
+        self.telefone = telefone
+        self.grupo = grupo
+        self.senha = senha
+
+    def selectALL(self):
+        banco = None
+        c = None
+
+        try:
+            banco = Banco()
+
+            c = banco.conexao.cursor()
+
+            _sql = "select id_funcionario, nome, matricula, cpf, telefone, grupo, senha from tb_funcionario"
+
+            _sql_data = ()
+
+            c.execute(_sql, _sql_data)
+
+            result = c.fetchall()
+
+            return result
+
+        except Exception as e:
+            return "Ocorreu um erro na busca do funcionário"
+
+        finally:
+            if c:
+                c.close()
+            if banco:
+                banco.conexao.close()
+    
+
+    def selectONE(self):
+        banco = None
+        c = None
+
+        try:
+            banco = Banco()
+
+            c = banco.conexao.cursor()
+
+            _sql = "select id_funcionario, nome, matricula, cpf, telefone, grupo, senha from tb_funcionario where id_funcionario = %s"
+
+            _sql_data = (self.id_funcionario,)
+
+            c.execute(_sql, _sql_data)
+
+            for linha in c:
+
+                self.id_funcionario = linha[0]
+                self.nome = linha[1]
+                self.matricula = linha[2]
+                self.cpf = linha[3]
+                self.telefone = linha[4]
+                self.grupo = linha[5]
+                self.senha = linha[6]
+
+            return "Busca feita com sucesso!"
+
+        except:
+            return "Ocorreu um erro na busca do funcionário"
+
+        finally:
+            if c:
+                c.close()
+            if banco:
+                banco.conexao.close()
+
+    def insert(self):
+        banco = None
+        c = None
+
+        try:
+            banco = Banco()
+
+            c = banco.conexao.cursor()
+
+            _sql = "insert into tb_funcionario(nome,matricula,cpf,telefone,grupo,senha) values (%s,%s,%s,%s,%s,%s)"
+
+            _sql_data = (self.nome, self.matricula, self.cpf,
+                         self.telefone, self.grupo, self.senha,)
+
+            c.execute(_sql, _sql_data)
+
+            banco.conexao.commit()
+
+            return "Funcionário cadastrado com sucesso!"
+
+        except Exception as e:
+            raise Exception('Erro ao cadastrar Funcionário!', str(e))
+        finally:
+            if c:
+                c.close
+            if banco:
+                banco.conexao.close()
+
+    def update(self):
+        banco = None
+        c = None
+
+        try:
+            banco = Banco()
+
+            c = banco.conexao.cursor()
+
+            _sql = "update tb_funcionario set nome=%s, matricula=%s, cpf=%s, telefone=%s, grupo=%s, senha=%s where id_funcionario=%s"
+
+            _sql_data = (self.nome, self.matricula, self.cpf, self.telefone,
+                         self.grupo, self.senha, self.id_funcionario,)
+
+            c.execute(_sql, _sql_data)
+
+            banco.conexao.commit()
+
+            return "Funcionário atualizado com sucesso!"
+
+        except Exception as e:
+            raise Exception("Erro ao editar Funcionário!", str(e))
+
+        finally:
+            if c:
+                c.close()
+            if banco:
+                banco.conexao.close()
+
+    def delete(self):
+        banco = None
+        c = None
+
+        try:
+            banco = Banco()
+
+            c = banco.conexao.cursor()
+
+            _sql = "delete from tb_funcionario where id_funcionario = %s"
+            _sql_data = (self.id_funcionario,)
+
+            c.execute(_sql,_sql_data)
+
+            banco.conexao.commit()
+
+            return "Funcionário excluído com sucesso!"
+
+        except Exception as e:
+            raise Exception("Erro ao tentar excluir Funcionário!", str(e))
+        
+        finally:
+            if c:
+                c.close()
+            if banco:
+                banco.conexao.close()
