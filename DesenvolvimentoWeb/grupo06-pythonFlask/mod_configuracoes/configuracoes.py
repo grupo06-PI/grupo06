6ce012e023014d0e807191cf3e2980af4c9ecd2c
nicoooadams <contato.nicoadams1@hotmail.com>
2020-11-02 21:03:40 -0300

    mod_configuracoes com ajax fix #54

--- DesenvolvimentoWeb/grupo06-pythonFlask/mod_configuracoes/configuracoes.py
+++ DesenvolvimentoWeb/grupo06-pythonFlask/mod_configuracoes/configuracoes.py
@@ -1,7 +1,40 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect, request, url_for, flash, jsonify
+
+from mod_configuracoes.configuracoesBD import Configuracoes
 
 bp_configuracoes = Blueprint('configuracoes', __name__, template_folder='templates')
 
-@bp_configuracoes.route("/Configuracoes", methods=['GET'])
+@bp_configuracoes.route("/Configuracoes", methods=['GET','POST'])
 def formConfiguracoes():
-    return render_template("formConfiguracoes.html")
+    configuracoes = Configuracoes()
+    return render_template("formConfiguracoes.html", configuracoes=configuracoes, content_type='application/json')
+
+
+@bp_configuracoes.route("/listaConfiguracoes", methods=['GET','POST'])
+def ListaConfiguracoes():
+    configuracoes = Configuracoes()
+    res = configuracoes.SelectALL()
+    return render_template("formListaConfiguracoes.html", result=res, content_type='application/json')
+
+@bp_configuracoes.route('/formEditConfiguracoes', methods=['POST'])
+def formEditConfiguracoes():
+    configuracoes=Configuracoes()
+    configuracoes.selectONE()
+    return render_template('formConfiguracoes.html', configuracoes=configuracoes, content_type='application/json')
+
+
+@bp_configuracoes.route('/editConfiguracoes', methods=['POST'])
+def editConfiguracoes():
+    _msg = ""
+    try:
+
+        configuracoes = Configuracoes()
+        configuracoes.taxa_juro_diario = request.form['taxa_juro_diario']
+        configuracoes.multa_atraso = request.form['multa_atraso']
+      
+        _msg = configuracoes.update()
+        return jsonify(erro=False, mensagem=_msg)
+
+    except Exception as e:
+        _msg, _msg_exception = e.args
+        return jsonify(erro=True, mensagem=_msg, mensagem_exception=_msg_exception)
